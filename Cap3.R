fd9bc2d32902efcb34eb1e4e27a3202907192737
Lexhb <alejandrohb77@hotmail.com>
2025-01-07 15:24:06 -0600

    Funciones filter y select, capítulo 3

--- Cap3.R
+++ Cap3.R
@@ -1,5 +1,9 @@
 atencion_ciudadano <- read.csv("http://bitsandbricks.github.io/data/gcba_suaci_barrios.csv")
 
+install.packages("tidyverse")
+
+library(tidyverse)
+
 str(atencion_ciudadano)
 
 summary(atencion_ciudadano)
@@ -21,5 +25,45 @@ atencion_ciudadano <- read.csv("atencion_ciudadano.csv")
 getwd()
 
 seleccion <- select(atencion_ciudadano, PERIODO, total)
+
+head(seleccion)
+
+seleccion <- select(atencion_ciudadano, RUBRO:BARRIO)
+
+head(seleccion)
+
+seleccion <- select(atencion_ciudadano, -RUBRO)
+
+seleccion <- select(atencion_ciudadano, -(TIPO_PRESTACION:total))
+
+seleccion <- select(atencion_ciudadano, -RUBRO, -BARRIO)
+
+seleccion <- filter(atencion_ciudadano, BARRIO == "RETIRO", PERIODO == 201401)
+
+head(seleccion)
+
+str(seleccion)
+
+dim(seleccion)
+
+seleccion <- filter(atencion_ciudadano, total>100)
+
+head(seleccion)
+
+seleccion <- filter(atencion_ciudadano, PERIODO==201508, RUBRO=="SALUD")
+
+head(seleccion)
+
+seleccion <- filter(atencion_ciudadano, BARRIO == "RETIRO" | BARRIO == "PALERMO")
+
+head(seleccion)
+
+seleccion <- filter(atencion_ciudadano, 
+                    TIPO_PRESTACION == "TRAMITE" & !(RUBRO == "REGISTRO CIVIL"))
+
+head(seleccion)
+
+seleccion <- filter(atencion_ciudadano, 
+                    !(TIPO_PRESTACION == "DENUNCIA" & RUBRO == "SEGURIDAD E HIGIENE"))
 
 head(seleccion)
