a7a1f712c895cec357d55343b57c75c32b236296
Lexhb <alejandrohb77@hotmail.com>
2025-01-08 12:05:53 -0600

    Se cubrió en su totalidad

--- Cap3.R
+++ Cap3.R
@@ -67,3 +67,69 @@ seleccion <- filter(atencion_ciudadano,
                     !(TIPO_PRESTACION == "DENUNCIA" & RUBRO == "SEGURIDAD E HIGIENE"))
 
 head(seleccion)
+
+ordenado <- arrange(atencion_ciudadano, total)
+
+head(ordenado)
+
+ordenado <- arrange(atencion_ciudadano, total, BARRIO)
+
+ordenado <- arrange(atencion_ciudadano, desc(total))
+
+## MUTATE
+
+circulos <- data.frame(nombre = c("Círculo 1", "Círculo 2", "Círculo 3"),
+                       tamaño = c("Pequeño", "Mediano", "Grande"),
+                       radio = c(1, 3, 5))
+circulos
+
+mutate(circulos, area = 3.1416 * radio^2) 
+
+atencion_ciudadano <- mutate(atencion_ciudadano,
+                             AÑO = substr(PERIODO, 1, 4),
+                             MES = substr(PERIODO, 5, 6))
+
+head(atencion_ciudadano)
+
+summarise(atencion_ciudadano, promedio = mean(total))
+
+agrupado <- group_by(atencion_ciudadano, AÑO)
+
+head(agrupado)
+
+summarise(agrupado, promedio_totales = mean(total))
+
+agrupado <- group_by(atencion_ciudadano, AÑO, MES)
+
+sumario <- summarise(agrupado, promedio = mean(total))
+
+head(sumario)
+
+agrupado <- group_by(atencion_ciudadano, AÑO, MES, BARRIO)
+
+sumario <- summarise(agrupado, promedio = mean(total))
+
+head(sumario)
+
+## PIPE
+
+solo2014 <- filter(atencion_ciudadano, AÑO == 2014)
+
+head(solo2014)
+
+solo2014_agrupado_barrio <- group_by(solo2014, BARRIO)
+
+total_por_barrio_2014 <- summarise(solo2014_agrupado_barrio, total = sum(total))
+
+head(total_por_barrio_2014)
+
+total_por_barrio_2014_ordenado <- arrange(total_por_barrio_2014, desc(total))
+
+head(total_por_barrio_2014_ordenado, 5)
+
+atencion_ciudadano %>%
+  filter(AÑO == 2014) %>%
+  group_by(BARRIO) %>%
+  summarise(total = sum(total)) %>%
+  arrange(desc(total)) %>%
+  head(5)
